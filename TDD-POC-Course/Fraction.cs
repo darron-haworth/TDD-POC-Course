1ce1cf55d9f37cacbf47423f89946f381678c1ba
Darron Haworth <darron.haworth@firsttechfed.com>
2017-03-31 10:58:17 -0700

    start over with Fraction tests/classes - start with failing test

---
+++ TDD-POC-Course/Fraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_POC_Course
+{
+    public class Fraction
+    {
+
+        public int Numerator { get; set; }
+        public int Denominator { get; set; }
+
+           
+
+        public  Fraction(int intValue)
+        {
+            Numerator = intValue;
+            Denominator = 1;
+        }
+
+        public Fraction Plus(Fraction fraction)
+        {
+            var retFraction = new Fraction(1);
+            return retFraction;
+        }
+
+    }
+}
---
+++ UnitTestProject/AddFractionsTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD_POC_Course;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class AddFractionsTest
+    {
+        [TestMethod]
+        public void zeroPlusZero()
+        {
+            var sum = new Fraction(0).Plus(new Fraction(0));
+ 
+            Assert.IsTrue(sum.Numerator  == 0);
+        }
+    }
+}
