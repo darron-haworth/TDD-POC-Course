661302cf761dcce7d6b34e48dd824edd3f85cb10
Darron Haworth <darron.haworth@firsttechfed.com>
2017-04-01 12:40:40 -0700

    refactor test to use Assert.AreEqual

--- UnitTestProject/AddFractionsTest.cs
+++ UnitTestProject/AddFractionsTest.cs
@@ -12,43 +12,43 @@ namespace UnitTestProject
         [TestMethod]
         public void zeroPlusZero()
         {
-            Assert.IsTrue(new Fraction(0).Equals(new Fraction(0).Plus(new Fraction(0))));
+            Assert.AreEqual(new Fraction(0), new Fraction(0).Plus(new Fraction(0)));
         }
 
         [TestMethod]
         public void nonZeroPlusZero()
         {
-            Assert.IsTrue(new Fraction(5).Equals(new Fraction(5).Plus(new Fraction(0))));
+            Assert.AreEqual(new Fraction(5), new Fraction(5).Plus(new Fraction(0)));
         }
 
         [TestMethod]
         public void zeroPlusNonZero()
         {
-            Assert.IsTrue(new Fraction(3).Equals(new Fraction(0).Plus(new Fraction(3))));
+            Assert.AreEqual(new Fraction(3), new Fraction(0).Plus(new Fraction(3)));
         }
 
         [TestMethod]
         public void nonZeroPlusNonZero()
         {
-            Assert.IsTrue(new Fraction(5).Plus(new Fraction(8)).Equals(new Fraction(13)));
+            Assert.AreEqual(new Fraction(5).Plus(new Fraction(8)), new Fraction(13));
         }
 
         [TestMethod]
         public void negativeNonZeroPlusZero()
         {
-            Assert.IsTrue(new Fraction(-3).Equals(new Fraction(-3).Plus(new Fraction(0))));
+            Assert.AreEqual(new Fraction(-3), new Fraction(-3).Plus(new Fraction(0)));
         }
 
         [TestMethod]
         public void zeroPlusNegativeNotZero()
         {
-            Assert.IsTrue(new Fraction(-3).Equals(new Fraction(0).Plus(new Fraction(-3))));
+            Assert.AreEqual(new Fraction(-3), new Fraction(0).Plus(new Fraction(-3)));
         }
 
         [TestMethod]
         public void nonTrivialButCommonDenominator()
         {
-            Assert.IsTrue(new Fraction(4, 5).Equals(new Fraction(1, 5).Plus(new Fraction(3, 5))));
+            Assert.AreEqual(new Fraction(4, 5), new Fraction(1, 5).Plus(new Fraction(3, 5)));
         }
     }
 }
