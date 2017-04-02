425a3d1754987472f4e1d1227de34fb0cfff9c5a
Darron Haworth <darron.haworth@firsttechfed.com>
2017-04-02 10:26:44 -0700

    clean up FractionEquals Tests

--- UnitTestProject/FractionEqualsTest.cs
+++ UnitTestProject/FractionEqualsTest.cs
@@ -12,30 +12,30 @@ namespace UnitTestProject
         [TestMethod]
         public void sameNumeratorAndDenominator()
         {
-            Assert.IsTrue(new Fraction(3, 5).Equals(new Fraction(3,5)));
+            Assert.AreEqual(new Fraction(3, 5), new Fraction(3,5));
         }
 
         [TestMethod]
         public void differentNumerators()
         {
-            Assert.IsFalse(new Fraction(2, 5).Equals(new Fraction(1, 5)));
+            Assert.AreNotEqual(new Fraction(2, 5), new Fraction(1, 5));
         }
 
         [TestMethod]
         public void differentDenominators()
         {
-            Assert.IsFalse(new Fraction(2, 5).Equals(new Fraction(1, 8)));
+            Assert.AreNotEqual(new Fraction(2, 5), new Fraction(1, 8));
         }
 
         [TestMethod] public void wholeNumberEqualsSameFraction()
         {
-            Assert.IsTrue(new Fraction(5).Equals(new Fraction(5, 1)));
+            Assert.AreEqual(new Fraction(5), new Fraction(5, 1));
         }
 
         [TestMethod]
         public void wholeNumbersNotEqualToDifferentWholeNumber()
         {
-            Assert.IsFalse(new Fraction(5).Equals(new Fraction(6)));
+            Assert.AreNotEqual(new Fraction(5), new Fraction(6));
         }
 
         [TestMethod]
