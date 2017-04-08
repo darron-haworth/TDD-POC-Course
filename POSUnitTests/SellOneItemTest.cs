c158807bc5a0e83f6961b16091c7b817bab82089
Darron Haworth <darron.haworth@firsttechfed.com>
2017-04-07 19:39:39 -0700

    refactored project to inlude barcode section

---
+++ POSUnitTests/SellOneItemTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace POSUnitTests
+{
+    [TestClass]
+    public class SellOneItemTest
+    {
+        [TestMethod]
+        public void ProductFound()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ SellOneItemNotes.md
@@ -0,0 +1,8 @@
+# Sell One Item
+
+Use cases
+
+product found
+product not found
+empty barcode
+null barcode
